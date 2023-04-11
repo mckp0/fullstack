212f6068c5ed2fe277492c0cb22d7f4f99db84f4
mckp0 <michaelchunkp@gmail.com>
2023-04-11 09:11:31 +0800

    part1-anecdotes save

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+
+  const handleNext = () => {
+    let x = Math.floor(Math.random() * anecdotes.length)
+    console.log("anecdote selected:", x)
+    setSelected(x)
+  }
+
+  const handleVote = () => {
+    console.log("voted", selected)
+    const copy = [...points]
+    copy[selected] += 1
+    console.log(copy)
+    setPoints(copy)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div>      
+        {anecdotes[selected]}
+      </div> 
+      <div>
+        has {points[selected]} votes
+      </div>
+      <div>
+        <Button handleClick={handleVote} text="vote" />
+        <Button handleClick={handleNext} text="next anecdote" />
+      </div>
+      <h1>Anecdote with most votes</h1>
+      {points}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
